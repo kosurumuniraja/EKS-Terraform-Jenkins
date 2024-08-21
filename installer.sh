4d1c9522ff81f01a8c1396c712ef2d4113a2bb4d
kosurumuniraja <86332515+kosurumuniraja@users.noreply.github.com>
2024-08-21 13:10:49 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,8 @@
 # Java installation for jenkins
 
 sudo apt update
-sudo apt install openjdk-11-jre -y
+sudo apt install openjdk-17-jdk -y
+sudo update-alternatives --config java
 
 # Jenkins installation 
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
